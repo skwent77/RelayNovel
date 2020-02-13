158eef0adfd5afb64509354fa7d2cd60609c7c42
JaeHyunShin <skwent77@gmail.com>
2020-02-14 03:24:55 +0900

    jaeHyun in HackertonDay_activities' 4 layouts linked with intents and almost done with ClickListener

--- app/src/main/java/com/example/blockchain_v1/MainActivity.java
+++ app/src/main/java/com/example/blockchain_v1/MainActivity.java
@@ -33,8 +33,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
         btnIntoWriting=findViewById(R.id.button);
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
